9e8d61b7320b323417954fe82ce5db37d9ab32c5
Anika21-NextIn <anika21.nextin@gmail.com>
2023-11-29 03:01:34 +0600

    upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/21535164-bbea7099-fb92-4420-b1b2-4c31a410211d?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
